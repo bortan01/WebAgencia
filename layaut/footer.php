6bf5e229f71cbd720a4167ade998f1f6d84d31de
Boris Ricardo Miranda <brmiranda009@gmail.com>
2021-05-21 15:08:46 -0600

    integrando dise;os

---
+++ layaut/footer.php
@@ -0,0 +1,132 @@
+<!-- ======= Footer ======= -->
+<footer id="contact">
+   <div class="footer-area">
+      <div class="container caja-footer">
+         <div class="row">
+            <div class="col-md-4 col-sm-4 col-xs-12">
+               <div class="footer-content">
+                  <div class="footer-head">
+                     <div class="footer-logo">
+                        <h2><span>Martínez</span>Travels & Tours</h2>
+                     </div>
+
+                     <p>Siguenos en todas nuestras redes sociales para estar al tanto de todas nuestras promociones y
+                        productos</p>
+                     <div class="footer-icons">
+                        <ul>
+                           <li>
+                              <a href="#"><i class="fa fa-facebook"></i></a>
+                           </li>
+                           <li>
+                              <a href="#"><i class="fa fa-twitter"></i></a>
+                           </li>
+                           <li>
+                              <a href="#"><i class="fa fa-google"></i></a>
+                           </li>
+                           <li>
+                              <a href="#"><i class="fa fa-pinterest"></i></a>
+                           </li>
+                        </ul>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <!-- end single footer -->
+            <div class="col-md-4 col-sm-4 col-xs-12">
+               <div class="footer-content">
+                  <div class="footer-head">
+                     <div class="footer-logo">
+                        <h2><span>Cont</span>acto</h2>
+                     </div>
+                     <p>
+                        Tambien puedes contactarnos de las siguiente manera
+                     </p>
+                     <div class="footer-contacts">
+                        <p><span>Tel:</span>+(503) 7841-1184</p>
+                        <p><span>Email:</span>info.ventas@martineztraveltours.com</p>
+
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <!-- end single footer -->
+            <div class="col-md-4 col-sm-4 col-xs-12">
+               <div class="footer-content">
+                  <div class="footer-head">
+                     <div class="footer-logo">
+                        <h2><span>Ubic</span>ación</h2>
+                     </div>
+                     <p>
+                        Ubicación: 2a Avenida Sur, Barrio el Centro #4D San Vicente, El Salvador
+                     </p>
+                     <div class="footer-contacts">
+
+                        <p><span>Lunes-Viernes</span> (8am-5:30pm)</p>
+                        <p><span>Sábados</span> (8am-12:30pm)</p>
+                     </div>
+
+
+                  </div>
+               </div>
+            </div>
+         </div>
+         <br>
+
+
+
+         <div class="container">
+            <div class="row">
+               <!-- Start Google Map -->
+               <div class="col-md-12 col-sm-12 col-xs-12">
+                  <!--PONDRE EL CHAT-->
+                  <link rel="stylesheet"
+                     href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="footer-area-bottom">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+               <div class="copyright text-center">
+                  <p>
+                     &copy; Copyright <strong>eBusiness</strong>. All Rights Reserved
+                  </p>
+               </div>
+               <div class="credits">
+                  <!--
+              All the links in the footer should remain intact.
+              You can delete the links only if you purchased the pro version.
+              Licensing information: https://bootstrapmade.com/license/
+              Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=eBusiness
+            -->
+                  Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</footer>
+<!-- End  Footer -->
+
+<a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+<div id="preloader"></div>
+
+<!-- Vendor JS Files -->
+<script src="../assets/vendor/jquery/jquery.min.js"></script>
+<script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+<script src="../assets/vendor/php-email-form/validate.js"></script>
+<script src="../assets/vendor/appear/jquery.appear.js"></script>
+<script src="../assets/vendor/knob/jquery.knob.js"></script>
+<script src="../assets/vendor/parallax/parallax.js"></script>
+<script src="../assets/vendor/wow/wow.min.js"></script>
+<script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="../assets/vendor/nivo-slider/js/jquery.nivo.slider.js"></script>
+<script src="../assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+<script src="../assets/vendor/venobox/venobox.min.js"></script>
+
+<!-- Template Main JS File -->
+<script src="../assets/js/main.js"></script>
---
+++ layaut/menu.php
@@ -0,0 +1,49 @@
+</head>
+
+<body data-spy="scroll" data-target="#navbar-example" style="">
+   <!-- ======= Header ======= -->
+   <header id="header" class="fixed-top">
+      <div class="container d-flex">
+         <div class="logo mr-auto">
+            <h1 class="text-light">
+               <a href="index.html"><span>Martínez</span>Travels & Tours</a>
+            </h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+         </div>
+
+         <nav class="nav-menu d-none d-lg-block">
+            <ul>
+               <li class="active"><a href="#header">Inicio</a></li>
+               <li><a href="#about">¿Quienes somos?</a></li>
+               <li class="drop-down">
+                  <a href="">Servicios</a>
+                  <ul>
+                     <li>
+                        <a href="vistas/asesoria/">Asesoria Migratora</a>
+                     </li>
+                     <li>
+                        <a href="vistas/paquete/">Paquetes</a>
+                     </li>
+                     <li>
+                        <a href="vistas/tours/">Tours</a>
+                     </li>
+                     <li>
+                        <a href="vistas/vehiculos/">Renta de Vehiculos</a>
+                     </li>
+                     <li>
+                        <a href="vistas/encomienda/">Encomienda</a>
+                     </li>
+                     <li>
+                        <a href="vistas/vuelos">Vuelos</a>
+                     </li>
+                  </ul>
+               </li>
+               <li><a href="#contact">Contacto</a></li>
+               <li><a href="#contact">Resgistrate</a></li>
+            </ul>
+         </nav>
+         <!-- .nav-menu -->
+      </div>
+   </header>
+   <!-- End Header -->
