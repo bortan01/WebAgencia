901a6dfda605755205b0da991fdd7ef559c5c011
Abigail <pineverdi@gmail.com>
2021-11-29 10:58:00 -0600

    ups

--- servicios/vehiculos/disponibilidad.php
+++ servicios/vehiculos/disponibilidad.php
@@ -98,7 +98,7 @@ h6 {
                            <h3 class="my-3" name="mode1" id="mode1"></h3>
                            <h3 name="anio" id="anio"></h3>
                            <br>
-                           <h6>Categoria del Vehiculo: </h6>
+                           <h6>Categoría del Vehiculo: </h6>
                            <h6 name="categoria" id="categoria"></h6>
                            <hr>
                            <button class="btn btn-primary btn-block mb-3">
