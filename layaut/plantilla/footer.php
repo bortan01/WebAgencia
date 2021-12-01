6212aed8983925b67bb74a71eb14a2edbcef025d
Abigail <pineverdi@gmail.com>
2021-11-30 21:30:22 -0600

    corrigiendo footer

--- layaut/plantilla/footer.php
+++ layaut/plantilla/footer.php
@@ -1,104 +1,112 @@
 <!-- ======= Footer ======= -->
 <footer id="contact">
-   <div class="footer-area">
-      <div class="container caja-footer">
-         <div class="row">
-            <div class="col-md-4 col-sm-4 col-xs-12">
-               <div class="footer-content">
-                  <div class="footer-head">
-                     <div class="footer-logo">
-                        <h2><span>Martínez</span>Travels & Tours</h2>
-                     </div>
-
-                     <p>Síguenos en todas nuestras redes sociales para estar al tanto de todas nuestras promociones y
-                        productos</p>
-                     <div class="footer-icons">
-                        <ul>
-                           <li>
-                              <a href="#"><i class="fa fa-facebook"></i></a>
-                           </li>
-                           <li>
-                              <a href="#"><i class="fa fa-twitter"></i></a>
-                           </li>
-                           <li>
-                              <a href="#"><i class="fa fa-google"></i></a>
-                           </li>
-                           <li>
-                              <a href="#"><i class="fa fa-pinterest"></i></a>
-                           </li>
-                        </ul>
-                     </div>
-                  </div>
-               </div>
-            </div>
-            <!-- end single footer -->
-            <div class="col-md-4 col-sm-4 col-xs-12">
-               <div class="footer-content">
-                  <div class="footer-head">
-                     <div class="footer-logo">
-                        <h2><span>Cont</span>acto</h2>
-                     </div>
-                     <p>
-                        También puedes contactarnos de las siguiente manera
-                     </p>
-                     <div class="footer-contacts">
-                        <p><span>Teléfono:</span>+(503) 7841-1184</p>
-                        <p><span>Email:</span>info.ventas@martineztraveltours.com</p>
-
-                     </div>
-                  </div>
-               </div>
-            </div>
-            <!-- end single footer -->
-            <div class="col-md-4 col-sm-4 col-xs-12">
-               <div class="footer-content">
-                  <div class="footer-head">
-                     <div class="footer-logo">
-                        <h2><span>Ubic</span>ación</h2>
-                     </div>
-                     <p>
-                        Ubicación: 2a Avenida Sur, Barrio el Centro #4D San Vicente, El Salvador
-                     </p>
-                     <div class="footer-contacts">
-
-                        <p><span>Lunes a Viernes</span> (8:00 am a 5:30 pm)</p>
-                        <p><span>Sábados</span> (8:00 am a 12:30 pm)</p>
-                     </div>
-
-
-                  </div>
-               </div>
-            </div>
-         </div>
-         <br>
-
-
-
-         <div class="container">
+    <div class="footer-area">
+        <div class="container caja-footer">
             <div class="row">
-               <!-- Start Google Map -->
-               <div class="col-md-12 col-sm-12 col-xs-12">
-                  <!--PONDRE EL CHAT-->
-                  <link rel="stylesheet"
-                     href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-               </div>
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="footer-content">
+                        <div class="footer-head">
+                            <div class="footer-logo">
+                                <h2><span>Martínez</span>Travels & Tours</h2>
+                            </div>
+
+                            <p>Síguenos en todas nuestras redes sociales para estar al tanto de todas nuestras
+                                promociones y
+                                productos</p>
+                            <div class="footer-icons">
+                                <ul>
+                                    <li>
+                                        <a href="https://www.facebook.com/martineztours99/" style="padding-top: 10px"><i
+                                                class="fa fa-facebook"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="https://api.whatsapp.com/send/?phone=50378411184&text&app_absent=0"
+                                            style="padding-top: 10px"><i class="fa fa-whatsapp"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=info.ventas@martineztraveltours.com&su=Asunto&body=cuerpo%20mensaje&bcc=info.ventas@martineztraveltours.com."
+                                            style="padding-top: 10px"><i class="fa fa-google"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end single footer -->
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="footer-content">
+                        <div class="footer-head">
+                            <div class="footer-logo">
+                                <h2><span>Cont</span>acto</h2>
+                            </div>
+                            <p style="color: #444; font-weight: 200;">
+                              También puedes contactarnos de las siguiente manera:
+                           </p>
+                           <div class="footer-contacts">
+                              <p><span>Teléfono:</span>
+                                 <span style="color: #444; font-weight: 200;" name="telefono_a" id="telefono_a"></span>
+                              </p>
+                              <p><span>Email:</span>
+                                 <span style="color: #444; font-weight: 200;" name="email_a" id="email_a"></span>
+                              </p>
+                           </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end single footer -->
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="footer-content">
+                        <div class="footer-head">
+                            <div class="footer-logo">
+                                <h2><span>Ubic</span>ación</h2>
+                            </div>
+                            <div class="footer-contacts">
+                              <p>
+                                 <span>Ubicación: </span>
+                                 <span style="color: #444; font-weight: 200;" name="direccion_a"
+                                    id="direccion_a"></span>
+                              </p>
+                              <p><span name="semana_a" id="semana_a"></span>
+                                 <span style="color: #444; font-weight: 200;" name="horaS_a" id="horaS_a"></span>
+                              </p>
+                              <p><span name="fin_a" id="fin_a"></span>
+                                 <span style="color: #444; font-weight: 200;" name="horaF_a" id="horaF_a"></span>
+                              </p>
+                           </div>
+
+                        </div>
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-   </div>
-   <div class="footer-area-bottom">
-      <div class="container">
-         <div class="row">
-            <div class="col-md-12 col-sm-12 col-xs-12">
-               <div class="copyright text-center">
-                  <p>
-                     &copy; UES <strong>2021</strong>. Todos los derechos reservados
-                  </p>
-               </div>
+            <br>
+
+
+
+            <div class="container">
+                <div class="row">
+                    <!-- Start Google Map -->
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <!--PONDRE EL CHAT-->
+                        <link rel="stylesheet"
+                            href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-   </div>
+        </div>
+    </div>
+    <div class="footer-area-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="copyright text-center">
+                        <p>
+                            &copy; UES <strong>2021</strong>. Todos los derechos reservados
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
 <!-- End  Footer -->
 
@@ -125,6 +133,8 @@
 <script src="../../assets/vendor/nivo-slider/js/jquery.nivo.slider.js"></script>
 <script src="../../assets/vendor/owl.carousel/owl.carousel.min.js"></script>
 <script src="../../assets/vendor/venobox/venobox.min.js"></script>
+<script src="../../assets/js/controladores/agencia/mostrarInfo.js"></script>
+
 <!-- Template Main JS File -->
 <script src="../../assets/js/main.js"></script>
 <script src="../../assets/js/conf.js"></script>
