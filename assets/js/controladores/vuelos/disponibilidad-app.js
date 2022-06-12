6c20fd224a30d47704e38133350900cd87ecc3b2
Boris Ricardo Miranda <brmiranda009@gmail.com>
2022-06-12 11:29:43 -0600

    Update disponibilidad-app.js

--- assets/js/controladores/vuelos/disponibilidad-app.js
+++ assets/js/controladores/vuelos/disponibilidad-app.js
@@ -33,12 +33,12 @@ $(document).ready(function () {
                     imgBig.className = "product-image";
                     imgBig.src = galeria[index];
                     imagenGrande.appendChild(imgBig);
-                    let crear = $('#2' + index);
+                    let crear = $('#' + index);
                     crear.empty();
                     crear.append('<img src="' + galeria[index] + '" alt="">');
                     crear.show();
                 } else {
-                    let crear = $('#2' + index);
+                    let crear = $('#' + index);
                     crear.empty();
                     crear.append('<img src="' + galeria[index] + '" alt="">');
                     crear.show();
@@ -47,7 +47,7 @@ $(document).ready(function () {
             }
             for (let i = galeria.length; i <= 10; i++) {
                 //alert('aqui estoy');
-                $('#2' + i).hide();
+                $('#' + i).hide();
             }
 
 
